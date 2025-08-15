14500a983d07eb0c0bb443a083219b559affde01
SARTHAK ANDHARE <sarthakandhare000@gmail.com>
2025-08-15 22:58:54 +0530

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+# Reverse a string in Python
+
+# Take input from user
+string = input("Enter a string: ")
+
+# Method 1: Using slicing
+reversed_string = string[::-1]
+
+print("Reversed string:", reversed_string)
